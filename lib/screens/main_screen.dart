d854e5b84f6fc7fa9722d99009c40668f0b70fe0
nashva <nashvakizhakkayil@gmail.com>
2024-01-09 14:06:36 +0530

    initial commit

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Row(
+        children: [
+          Expanded(
+              child: Container(
+            color: Colors.amber,
+          ))
+        ],
+      )),
+    );
+  }
+}
