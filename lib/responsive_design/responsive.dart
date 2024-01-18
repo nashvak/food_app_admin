668147f60883aba4a6db5149b780dc9b667c8da2
nashva <nashvakizhakkayil@gmail.com>
2024-01-18 23:47:10 +0530

    making responsive

--- lib/responsive_design/responsive.dart
+++ lib/responsive_design/responsive.dart
@@ -1,44 +1,46 @@
 import 'package:flutter/material.dart';
 
-class Responsive extends StatelessWidget {
+class ResponsiveWidget extends StatelessWidget {
   final Widget mobile;
   final Widget tablet;
   final Widget desktop;
 
-  const Responsive({
+  const ResponsiveWidget({
     Key? key,
     required this.mobile,
     required this.tablet,
     required this.desktop,
   }) : super(key: key);
 
-  // static bool isMobile(BuildContext context) =>
-  //     MediaQuery.of(context).size.width < 850;
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width < 700;
+//<500
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width < 1200 &&
+      MediaQuery.of(context).size.width >= 700;
 
-  // static bool isTablet(BuildContext context) =>
-  //     MediaQuery.of(context).size.width < 1100 &&
-  //     MediaQuery.of(context).size.width >= 850;
-
-  // static bool isDesktop(BuildContext context) =>
-  //     MediaQuery.of(context).size.width >= 1100;
+//>=500&&<=1200
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1200;
+  //>1200
 
   @override
   Widget build(BuildContext context) {
     // final Size size = MediaQuery.of(context).size;
-    // if (size.width >= 1100) {
+    // if (size.width >= 1200) {
     //   return desktop;
-    // } else if (size.width >= 850 && tablet != null) {
-    //   return tablet;
+    // } else if (size.width >= 500 && size.width<=1200) {
+    //   return tablet??desktop;
     // } else {
-    //   return mobile;
+    //   return mobile??desktop;
     // }
     return LayoutBuilder(builder: (context, constraints) {
-      if (constraints.maxWidth < 700) {
-        return mobile;
-      } else if (constraints.maxWidth < 1100) {
+      if (constraints.maxWidth > 1200) {
+        return desktop;
+      } else if (constraints.maxWidth <= 1200 && constraints.maxWidth >= 700) {
         return tablet;
       } else {
-        return desktop;
+        return mobile;
       }
     });
   }
--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_app_admin/constants/app_colors.dart';
 import 'package:food_app_admin/constants/app_styles.dart';
+import 'package:food_app_admin/responsive_design/responsive.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -18,188 +19,196 @@ class LoginScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Expanded(
-              child: Container(
-                height: height,
-                color: appColor,
-                child: Center(
-                  child: Text(
-                    'Foodie',
-                    style: ralewayStyle.copyWith(
-                      fontSize: 48.0,
-                      color: white,
-                      fontWeight: FontWeight.w800,
+            ResponsiveWidget.isMobile(context)
+                ? const SizedBox()
+                : Expanded(
+                    child: Container(
+                      height: height,
+                      color: appColor,
+                      child: Center(
+                        child: Text(
+                          'Foodie',
+                          style: ralewayStyle.copyWith(
+                            fontSize: 48.0,
+                            color: white,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-            ),
             // SizedBox(
             //   width: width * 0.1,
             // ),
             Expanded(
               child: Container(
                 height: height,
-                margin: EdgeInsets.symmetric(horizontal: height * 0.20),
+                margin: EdgeInsets.symmetric(
+                    horizontal: ResponsiveWidget.isMobile(context)
+                        ? height * 0.032
+                        : height * 0.12),
                 color: white,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(height: height * 0.2),
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                            text: "Let's ",
-                            style: ralewayStyle.copyWith(
-                              fontSize: 25.0,
-                              color: appColor,
-                              fontWeight: FontWeight.normal,
+                child: SingleChildScrollView(
+                  padding: EdgeInsets.only(bottom: 40.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(height: height * 0.2),
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Let's ",
+                              style: ralewayStyle.copyWith(
+                                fontSize: 25.0,
+                                color: appColor,
+                                fontWeight: FontWeight.normal,
+                              ),
                             ),
-                          ),
-                          TextSpan(
-                            text: 'Sign In 👇',
-                            style: ralewayStyle.copyWith(
-                              fontWeight: FontWeight.w800,
-                              color: textColor,
-                              fontSize: 25.0,
+                            TextSpan(
+                              text: 'Sign In 👇',
+                              style: ralewayStyle.copyWith(
+                                fontWeight: FontWeight.w800,
+                                color: textColor,
+                                fontSize: 25.0,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    SizedBox(height: height * 0.02),
-                    Text(
-                      'Hey, Enter your details to get sign in \nto your account.',
-                      style: ralewayStyle.copyWith(
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.w400,
-                        color: textColor,
-                      ),
-                    ),
-                    SizedBox(height: height * 0.064),
-                    Text(
-                      'Email',
-                      style: ralewayStyle.copyWith(
-                        fontSize: 12.0,
-                        color: appColor,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    const SizedBox(height: 6.0),
-                    Container(
-                      height: 50.0,
-                      width: width,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16.0),
-                        color: white,
-                      ),
-                      child: TextFormField(
+                      SizedBox(height: height * 0.02),
+                      Text(
+                        'Hey, Enter your details to get sign in \nto your account.',
                         style: ralewayStyle.copyWith(
-                          fontWeight: FontWeight.w400,
-                          color: appColor,
                           fontSize: 12.0,
-                        ),
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          prefixIcon: Icon(
-                            Icons.email,
-                            size: 20,
-                          ),
-                          contentPadding: const EdgeInsets.only(top: 16.0),
-                          hintText: 'Enter Email',
-                          hintStyle: ralewayStyle.copyWith(
-                            fontWeight: FontWeight.w400,
-                            color: appColor.withOpacity(0.5),
-                            fontSize: 12.0,
-                          ),
+                          fontWeight: FontWeight.w400,
+                          color: textColor,
                         ),
                       ),
-                    ),
-                    SizedBox(height: height * 0.014),
-                    Text(
-                      'Password',
-                      style: ralewayStyle.copyWith(
-                        fontSize: 12.0,
-                        color: appColor,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    const SizedBox(height: 6.0),
-                    Container(
-                      height: 50.0,
-                      width: width,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16.0),
-                        color: white,
-                      ),
-                      child: TextFormField(
+                      SizedBox(height: height * 0.064),
+                      Text(
+                        'Email',
                         style: ralewayStyle.copyWith(
-                          fontWeight: FontWeight.w400,
-                          color: appColor,
                           fontSize: 12.0,
-                        ),
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          suffixIcon: Icon(
-                            Icons.visibility_off,
-                          ),
-                          prefixIcon: Icon(
-                            Icons.lock,
-                            size: 20,
-                          ),
-                          contentPadding: const EdgeInsets.only(top: 16.0),
-                          hintText: 'Enter Password',
-                          hintStyle: ralewayStyle.copyWith(
-                            fontWeight: FontWeight.w400,
-                            color: appColor.withOpacity(0.5),
-                            fontSize: 12.0,
-                          ),
+                          color: appColor,
+                          fontWeight: FontWeight.w700,
                         ),
                       ),
-                    ),
-                    SizedBox(height: height * 0.03),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Forgot Password?',
+                      const SizedBox(height: 6.0),
+                      Container(
+                        height: 50.0,
+                        width: width,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16.0),
+                          color: white,
+                        ),
+                        child: TextFormField(
                           style: ralewayStyle.copyWith(
+                            fontWeight: FontWeight.w400,
+                            color: appColor,
                             fontSize: 12.0,
-                            color: appColor,
-                            fontWeight: FontWeight.w600,
                           ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: height * 0.05),
-                    Material(
-                      color: Colors.transparent,
-                      child: InkWell(
-                        onTap: () {},
-                        borderRadius: BorderRadius.circular(16.0),
-                        child: Ink(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 70.0, vertical: 18.0),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16.0),
-                            color: appColor,
-                          ),
-                          child: Text(
-                            'Sign In',
-                            style: ralewayStyle.copyWith(
-                              fontWeight: FontWeight.w700,
-                              color: white,
-                              fontSize: 16.0,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon: Icon(
+                              Icons.email,
+                              size: 20,
+                            ),
+                            contentPadding: const EdgeInsets.only(top: 16.0),
+                            hintText: 'Enter Email',
+                            hintStyle: ralewayStyle.copyWith(
+                              fontWeight: FontWeight.w400,
+                              color: appColor.withOpacity(0.5),
+                              fontSize: 12.0,
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(height: height * 0.014),
+                      Text(
+                        'Password',
+                        style: ralewayStyle.copyWith(
+                          fontSize: 12.0,
+                          color: appColor,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      const SizedBox(height: 6.0),
+                      Container(
+                        height: 50.0,
+                        width: width,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16.0),
+                          color: white,
+                        ),
+                        child: TextFormField(
+                          style: ralewayStyle.copyWith(
+                            fontWeight: FontWeight.w400,
+                            color: appColor,
+                            fontSize: 12.0,
+                          ),
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            suffixIcon: Icon(
+                              Icons.visibility_off,
+                            ),
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              size: 20,
+                            ),
+                            contentPadding: const EdgeInsets.only(top: 16.0),
+                            hintText: 'Enter Password',
+                            hintStyle: ralewayStyle.copyWith(
+                              fontWeight: FontWeight.w400,
+                              color: appColor.withOpacity(0.5),
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: height * 0.03),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Forgot Password?',
+                            style: ralewayStyle.copyWith(
+                              fontSize: 12.0,
+                              color: appColor,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: height * 0.05),
+                      Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () {},
+                          borderRadius: BorderRadius.circular(16.0),
+                          child: Ink(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 70.0, vertical: 18.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16.0),
+                              color: appColor,
+                            ),
+                            child: Text(
+                              'Sign In',
+                              style: ralewayStyle.copyWith(
+                                fontWeight: FontWeight.w700,
+                                color: white,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
